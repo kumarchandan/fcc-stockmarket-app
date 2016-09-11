9b5d6eb2880366916289d1f74672388fa61313f6
K.Chan <chandankumar99341@gmail.com>
2016-09-12 00:51:45 +0530

    add dummy mongodb url

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 // database.js
 
 module.exports = {
-    mongoURL: 'mongodb://localhost:27017/stockmarket-app'
+    // mongoURL: 'mongodb://localhost:27017/stockmarket-app'
+    mongoURL: 'mongodb://kchan:99341@ds019816.mlab.com:19816/party-app'
 }
