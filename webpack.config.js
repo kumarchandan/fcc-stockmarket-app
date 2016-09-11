14553da9b4a91f7de46906763291ca79cb19a88b
K.Chan <chandankumar99341@gmail.com>
2016-09-12 01:13:41 +0530

    fix bug

--- webpack.config.js
+++ webpack.config.js
@@ -31,15 +31,15 @@ module.exports = {
         ]
     }
     // Uncomment while uploading
-    // plugins: [
-    //     new webpack.optimize.UglifyJsPlugin({
-    //         minimize: true,
-    //         compress: {
-    //             warnings: false
-    //         },
-    //         output: {
-    //             comments: false
-    //         }
-    //     })
-    // ]
+    ,plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false
+            },
+            output: {
+                comments: false
+            }
+        })
+    ]
 }
